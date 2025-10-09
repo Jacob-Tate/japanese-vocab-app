ad52285399e3945446487f8cd2bed0a48ebb77db
Jacob Tate <jacob11163@gmail.com>
2025-10-09 00:34:13 -0700

    moved counters to hiragana

--- src/counters.js
+++ src/counters.js
@@ -7,8 +7,8 @@ export const counters = [
     uses: 'long, cylindrical objects',
     items: ['pencils', 'bottles', 'trees', 'umbrellas', 'guitars'],
     readings: {
-      1: 'ippon', 2: 'nihon', 3: 'sanbon', 4: 'yonhon', 5: 'gohon',
-      6: 'roppon', 7: 'nanahon', 8: 'happon', 9: 'kyuuhon', 10: 'juppon',
+      1: 'いっぽん', 2: 'にほん', 3: 'さんぼん', 4: 'よんほん', 5: 'ごほん',
+      6: 'ろっぽん', 7: 'ななほん', 8: 'はっぽん', 9: 'きゅうほん', 10: 'じゅっぽん',
     },
   },
   {
@@ -17,8 +17,8 @@ export const counters = [
     uses: 'people',
     items: ['students', 'friends', 'people', 'customers'],
     readings: {
-      1: 'hitori', 2: 'futari', 3: 'sannin', 4: 'yonin', 5: 'gonin',
-      6: 'rokunin', 7: 'nananin', 8: 'hachinin', 9: 'kyuunin', 10: 'juunin',
+      1: 'ひとり', 2: 'ふたり', 3: 'さんにん', 4: 'よにん', 5: 'ごにん',
+      6: 'ろくにん', 7: 'ななにん', 8: 'はちにん', 9: 'きゅうにん', 10: 'じゅうにん',
     },
   },
   {
@@ -27,8 +27,8 @@ export const counters = [
     uses: 'small animals (insects, fish, cats, dogs)',
     items: ['cats', 'dogs', 'fish', 'insects'],
     readings: {
-      1: 'ippiki', 2: 'nihiki', 3: 'sanbiki', 4: 'yonhiki', 5: 'gohiki',
-      6: 'roppiki', 7: 'nanahiki', 8: 'happiki', 9: 'kyuuhiki', 10: 'juppiki',
+      1: 'いっぴき', 2: 'にひき', 3: 'さんびき', 4: 'よんひき', 5: 'ごにき',
+      6: 'ろっぴき', 7: 'ななひき', 8: 'はっぴき', 9: 'きゅうにき', 10: 'じゅっぴき',
     },
   },
   {
@@ -37,8 +37,8 @@ export const counters = [
     uses: 'flat, thin objects',
     items: ['sheets of paper', 'stamps', 'shirts', 'plates'],
     readings: {
-      1: 'ichimai', 2: 'nimai', 3: 'sanmai', 4: 'yonmai', 5: 'gomai',
-      6: 'rokumai', 7: 'nanamai', 8: 'hachimai', 9: 'kyuumai', 10: 'juumai',
+      1: 'いちまい', 2: 'にまい', 3: 'さんまい', 4: 'よんまい', 5: 'ごまい',
+      6: 'ろくまい', 7: 'ななまい', 8: 'はちまい', 9: 'きゅうまい', 10: 'じゅうまい',
     },
   },
   {
@@ -47,8 +47,8 @@ export const counters = [
     uses: 'small and round objects',
     items: ['apples', 'eggs', 'pieces of candy', 'boxes'],
     readings: {
-      1: 'ikko', 2: 'niko', 3: 'sanko', 4: 'yonko', 5: 'goko',
-      6: 'rokko', 7: 'nanako', 8: 'hakko', 9: 'kyuuko', 10: 'jukko',
+      1: 'いっこ', 2: 'にこ', 3: 'さんこ', 4: 'よんこ', 5: 'ごこ',
+      6: 'ろっこ', 7: 'ななこ', 8: 'はっこ', 9: 'きゅうこ', 10: 'じゅっこ',
     },
   },
   {
@@ -57,8 +57,8 @@ export const counters = [
     uses: 'bound objects',
     items: ['books', 'magazines', 'notebooks'],
     readings: {
-      1: 'issatsu', 2: 'nisatsu', 3: 'sansatsu', 4: 'yonsatsu', 5: 'gosatsu',
-      6: 'rokusatsu', 7: 'nanasatsu', 8: 'hassatsu', 9: 'kyuusatsu', 10: 'jussatsu',
+      1: 'いっさつ', 2: 'にさつ', 3: 'さんさつ', 4: 'よんさつ', 5: 'ごさつ',
+      6: 'ろくさつ', 7: 'ななさつ', 8: 'はっさつ', 9: 'きゅうさつ', 10: 'じゅっさつ',
     },
   },
   {
@@ -67,8 +67,8 @@ export const counters = [
     uses: 'age',
     items: ['years old'],
     readings: {
-      1: 'issai', 2: 'nisai', 3: 'sansai', 4: 'yonsai', 5: 'gosai',
-      6: 'rokusai', 7: 'nanasai', 8: 'hassai', 9: 'kyuusai', 10: 'jussai',
+      1: 'いっさい', 2: 'にさい', 3: 'さんさい', 4: 'よんさい', 5: 'ごさい',
+      6: 'ろくさい', 7: 'ななさい', 8: 'はっさい', 9: 'きゅうさい', 10: 'じゅっさい',
     },
   },
   {
@@ -77,8 +77,8 @@ export const counters = [
     uses: 'minutes',
     items: ['minutes'],
     readings: {
-      1: 'ippun', 2: 'nifun', 3: 'sanpun', 4: 'yonpun', 5: 'gofun',
-      6: 'roppun', 7: 'nanafun', 8: 'happun', 9: 'kyuufun', 10: 'juppun',
+      1: 'いっぷん', 2: 'にふん', 3: 'さんぷん', 4: 'よんぷん', 5: 'ごふん',
+      6: 'ろっぷん', 7: 'ななふん', 8: 'はっぷん', 9: 'きゅうふん', 10: 'じゅっぷん',
     },
   },
 ];
